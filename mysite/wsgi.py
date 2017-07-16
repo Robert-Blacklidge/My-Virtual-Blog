9187bfaf4b0a55f220316b2a0fd0aaba9ef22542
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-16 08:58:34 -0400

    wsgi updates

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,26 +8,21 @@ import os
 import sys
 
 
-path = os.path.expanduser('~/My-Virtual-Blog')
+path = '/home/knight9954/My-Virtual-Blog'
 if path not in sys.path:
     sys.path.append(path)
-<<<<<<< HEAD
 
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'My-Virtual-Blog.settings'
 
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
 #application = get_wsgi_application()
-=======
+
 
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = get_wsgi_application()
-#application = StaticFilesHandler(get_wsgi_application())
 
->>>>>>> 8bdc53b8d1924adb99b3916a986b7c31cd41ebcb
