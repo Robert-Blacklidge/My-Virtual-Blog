e6da2438c15fa5c0c2ec43f067026fc05d165eb0
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-16 13:07:04 +0000

    wsgi update

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,7 +13,7 @@ if path not in sys.path:
     sys.path.append(path)
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'My-Virtual-Blog.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
 from django.core.wsgi import get_wsgi_application
