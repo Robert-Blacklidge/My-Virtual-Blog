466fb2c47e787a78211cc91863b7d5e5076ef943
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-15 20:02:45 -0400

    wsgi update

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,14 +13,16 @@ from django.core.wsgi import get_wsgi_application
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
 path = os.path.expanduser('~/My-Virtual-Blog')
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ("DJANGO_SETTINGS_MODULE", "My-Virtual-Blog.settings")
 
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
+
 application = StaticFilesHandler(get_wsgi_application())
 
 application = DjangoWhiteNoise(application)
