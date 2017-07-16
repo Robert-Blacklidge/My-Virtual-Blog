8d51a043a26db6784ea61ace2ed1b1b978b64404
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-16 11:06:04 -0400

    corrections

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,19 +1,24 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-
-path = '/home/knight9954/My-Virtual-Blog'
-if path not in sys.path:
-    sys.path.append(path)
-
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'My-Virtual-Blog.settings'
-
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
-#application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 
 
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+
+
+application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
+
