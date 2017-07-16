8bdc53b8d1924adb99b3916a986b7c31cd41ebcb
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-15 20:20:44 -0400

    wsgi update

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+
 
 
 import os
@@ -23,6 +23,6 @@ if path not in sys.path:
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
-
+application = get_wsgi_application()
+#application = StaticFilesHandler(get_wsgi_application())
 
